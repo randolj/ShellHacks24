e01f9a0a52b883ddacf75d7b87e750655967cbec
Narvaez Echeverria, Alex (FIU) <T0140999@miamidade.gov>
2024-09-28 02:57:21 -0400

    Created login frontend page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import logo from "./Capital_One_logo.png";
+import "./Login.css";
 
 function Login() {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <div className="body">
+      <img src={logo} alt="Capital One Logo" className="logo" />
+      {/* Centered form container with the text fields */}
+      <Box
+        className="form-box"
+        component="form"
+        sx={{ "& > :not(style)": { m: 1, width: "40ch" } }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-username" label="Username" variant="outlined" />
+        <TextField id="outlined-password" label="Password" variant="outlined" />
+      </Box>
+
+      {/* Sign In button positioned right below */}
+      <Button
+        className="center-button"
+        sx={{
+          backgroundColor: "rgb(71,140,209)",
+          color: "white",
+          "&:hover": {
+            backgroundColor: "rgb(0,72,120)",
+          },
+        }}
+        variant="contained"
+      >
+        Sign In
+      </Button>
+    </div>
   );
 }
 
