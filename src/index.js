cfce656bed073ff78c56b2cef10ea35bae613262
Matthew Larrosa <Matthewterra16@gmail.com>
2024-09-28 11:57:46 -0400

    budget section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./Components/Login/Login";
+import Budgeting from "./Components/Budgeting/Budgeting";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Budgeting/>
   </React.StrictMode>
 );
