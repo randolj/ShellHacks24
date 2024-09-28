be3b1085f7a463e50874b048e1891237b368ef08
Narvaez Echeverria, Alex (FIU) <T0140999@miamidade.gov>
2024-09-28 13:13:09 -0400

    Created brokerage based cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./Components/Login/Login";
+import Finance from "./Components/Finance/Finance";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Finance />
   </React.StrictMode>
 );
