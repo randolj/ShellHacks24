5ca2397d874d989e63830947ff9fa5ebc6a2ce50
randolj <r.recio@ufl.edu>
2024-09-28 09:20:15 -0400

    feat: signup and redirect to login/signup

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -3,10 +3,10 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import logo from "../Capital_One_logo.png";
-import "./Login.css";
 import {useNavigate} from "react-router-dom"
+import "./SignUp.css";
 
-const Login = () => {
+const SignUp = () => {
   const navigate = useNavigate();
 
   return (
@@ -22,11 +22,11 @@ const Login = () => {
       >
         <TextField id="outlined-username" label="Username" variant="outlined" />
         <TextField id="outlined-password" label="Password" variant="outlined" type="password" />
+        <TextField id="outlined-password" label="Repeat Password" variant="outlined" type="password" />
       </Box>
-      <div className="redirect" onClick={() => navigate("/signup")}>
-          <span>{"Don't have an account?"}</span>
+      <div className="redirect" onClick={() => navigate("/login")}>
+          <span>{"Already have an account?"}</span>
       </div>
-      {/* Sign In button positioned right below */}
       <Button
         className="center-button"
         sx={{
@@ -38,10 +38,10 @@ const Login = () => {
         }}
         variant="contained"
       >
-        Sign In
+        Sign Up
       </Button>
     </div>
   );
 }
 
-export default Login;
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./Components/Login/Login";
+import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
